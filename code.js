fc7bfeeff51098a4788143b36e86a9ee31dc2bae
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-11-19 11:25:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 function mergesort(array) {
     let n = array.length;
 
-    // Iteratively merge subarrays in bottom-up manner
+    
     for (let size = 1; size < n; size *= 2) {
         for (let leftStart = 0; leftStart < n - size; leftStart += 2 * size) {
             let mid = leftStart + size - 1;
@@ -16,12 +16,12 @@ function mergesort(array) {
 function mergeInPlace(array, left, mid, right) {
     let start2 = mid + 1;
 
-    // If the direct merge is already sorted
+    
     if (array[mid] <= array[start2]) {
         return;
     }
 
-    // Use two pointers to merge the subarrays in place
+  
     while (left <= mid && start2 <= right) {
         if (array[left] <= array[start2]) {
             left++;
@@ -29,7 +29,7 @@ function mergeInPlace(array, left, mid, right) {
             let value = array[start2];
             let index = start2;
 
-            // Shift elements to the right
+           
             while (index !== left) {
                 array[index] = array[index - 1];
                 index--;
@@ -43,5 +43,5 @@ function mergeInPlace(array, left, mid, right) {
     }
 }
 
-// Export the mergeSort function
+
 module.exports = mergesort;
